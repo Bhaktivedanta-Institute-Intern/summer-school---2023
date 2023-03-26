1251c85b67c50efe83918f1ecd1e77d7641f53f4
Vishal kumar <vishalk80269@gmail.com>
2023-03-26 17:13:58 +0530

    edit some text and script

--- javascript/chatBot/chatResponse.js
+++ javascript/chatBot/chatResponse.js
@@ -1,8 +1,31 @@
-const responseObj={
-    hello:'hey! how can i help you',
-    hey:'hey whats up ',
-    contact:"sk123@gmail.com",
-    
+// const responseObj={
+//     hello:'hey! how can i help you',
+//     hey:'hey whats up ',
+//     contact:"sk123@gmail.com",
+// }
 
+function response(userInput){
+    if(userInput.toLocaleLowerCase() == 'I Have Registered but did not get the confirmation mail'.toLocaleLowerCase()){
+        let ans='Please wait for 24 hours you will get the mail';
+return ans;
+    }
+    else if(userInput.toLocaleLowerCase() == 'How I can Register'.toLocaleLowerCase()){
+        let ans='Need to go the Register page';
+        return ans;
+    }
+    else if(userInput.toLocaleLowerCase() == 'How I Can Reach the venue from my Location'.toLocaleLowerCase()){
+        let ans='Will be update soon';
+        return ans;
+    }
+    else if(userInput.toLocaleLowerCase() == 'Who are the Speakers'.toLocaleLowerCase()){
+        let ans='Need to go to speakers page';
+        return ans;
+    }
+    else if(userInput.toLocaleLowerCase() == 'Can I Register without Accommodation ?'.toLocaleLowerCase()){
+        let ans='Contact below details';
+        return ans;
+    }else{
+        return 'please ask something else'
+    }
 }
 
