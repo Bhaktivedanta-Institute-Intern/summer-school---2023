8b769d8d8ed44f285b36ac09cdade012ccd9ad67
Vishal kumar <vishalk80269@gmail.com>
2023-04-04 03:55:32 +0530

    working on publication.html

--- javascript/chatBot.js
+++ javascript/chatBot.js
@@ -1,79 +1,94 @@
+const chatBox = document.querySelector(".chat_body");
+// const textInput = document.querySelector("#text_input");
+// const sends = document.querySelector(".send");
+const chatContainer = document.getElementById("chat_container");
+const logoChatbot = document.getElementById("logo_chatbot");
+const loading = document.querySelector(".loadingChatBot");
 
-const chatBox=document.querySelector('.chat_body');
-const textInput=document.querySelector('#text_input');
-const sends=document.querySelector('.send');
-const chatContainer=document.getElementById('chat_container');
-const logoChatbot=document.getElementById('logo_chatbot');
-const loading=document.querySelector('.loadingChatBot');
+// const renderUserMessage=()=>{
+//     console.log('value')
+//      const userInput=textInput.value;
+//      if(userInput==""){
+//         return;
+//      }
+//      renderMessage(userInput,"user");
+//      textInput.value="";
+//      toggleLoading(false);
+//      setTimeout(()=>{
+//         renderChatBotResponse(userInput);
+//         toggleLoading(true);
+//         setScrollPosition();
+//      },600);
+//  }
 
+// const renderChatBotResponse=(userInput)=>{
+// const res=getChatBotResponse(userInput);
+// renderMessage(res);
+// }
 
-const renderUserMessage=()=>{
-    console.log('value')
-     const userInput=textInput.value;
-     if(userInput==""){
-        return;
-     }
-     renderMessage(userInput,"user");
-     textInput.value="";
-     toggleLoading(false);
-     setTimeout(()=>{
-        renderChatBotResponse(userInput);
-        toggleLoading(true);
-        setScrollPosition();
-     },600);
- } 
- 
-const renderChatBotResponse=(userInput)=>{
-const res=getChatBotResponse(userInput);
-renderMessage(res);
+//  const renderMessage=(tex,type)=>{
+//     let className='user_message';
+//     if(type!="user"){
+//         className="chatbot_message";
+//     }
+//  const message=document.createElement('div');
+//  const textNode=document.createTextNode(tex);
+//  message.classList.add(className);
+//  message.append(textNode);
+//  chatBox.append(message);
+
+//  }
+
+//  const getChatBotResponse=(userInput)=>{
+//     return response(userInput);
+//     // return responseObj[userInput]==undefined?"please contact s@123gmail.com":responseObj[userInput];
+//  }
+
+const setScrollPosition = () => {
+  if (chatBox.scrollHeight > 0) {
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+};
+
+//  textInput.addEventListener('keyup',(e)=>{
+//     if(e.keyCode===13){
+//         renderUserMessage();
+//     }
+//  })
+// sends.addEventListener('click',renderUserMessage);
+
+function show() {
+  // console.log('working or not');
+  logoChatbot.style.display = "none";
+
+  chatContainer.style.display = "block";
 }
 
- const renderMessage=(tex,type)=>{
-    let className='user_message';
-    if(type!="user"){
-        className="chatbot_message";
-    }
- const message=document.createElement('div');
- const textNode=document.createTextNode(tex);
- message.classList.add(className);
- message.append(textNode);
- chatBox.append(message);
- 
- }
-
- const getChatBotResponse=(userInput)=>{
-    return response(userInput);
-    // return responseObj[userInput]==undefined?"please contact s@123gmail.com":responseObj[userInput];
- }
-
-const setScrollPosition=()=>{
-    if(chatBox.scrollHeight>0){
-        chatBox.scrollTop=chatBox.scrollHeight;
-    }
+function showNone() {
+  chatContainer.style.display = "none";
+  logoChatbot.style.display = "flex";
 }
 
- textInput.addEventListener('keyup',(e)=>{
-    if(e.keyCode===13){
-        renderUserMessage(); 
+const toggleLoading = (show) => {
+  loading.classList.toggle("hidechatBotAnimation", show);
+};
+
+const questions = document.getElementsByClassName("questions");
+const answer = document.getElementsByClassName("answer");
+// const iconArrow = document.getElementById("iconArrow");
+const iconArrow = document.querySelectorAll(".questions i");
+for (let i = 0; i < questions.length; i++) {
+  let boolean = true;
+  questions[i].addEventListener("click", () => {
+    if (boolean) {
+      answer[i].style.display = "none";
+      iconArrow[i].className = "bi bi-caret-down-fill";
+      boolean = false;
+    } else {
+      answer[i].style.display = "inline-block";
+      iconArrow[i].className = "bi bi-caret-up-fill";
+      boolean = true;
     }
- })
-sends.addEventListener('click',renderUserMessage);
-
-
-function show(){
-    // console.log('working or not');
-    logoChatbot.style.display='none'; 
-   
-    chatContainer.style.display='block';   
-   
+    setScrollPosition();
+  });
 }
-
-function showNone(){
-    chatContainer.style.display='none';
-    logoChatbot.style.display='flex';
-}
-
-const toggleLoading=(show)=>{
-    loading.classList.toggle('hidechatBotAnimation',show);
-    }
-
--- javascript/chatBot/chatResponse.js
+++ javascript/chatBot/chatResponse.js
@@ -4,42 +4,45 @@
 //     contact:"sk123@gmail.com",
 // }
 
-function response(userInput){
-    if(userInput.toLocaleLowerCase() == 'I Have Registered but did not get the confirmation mail'.toLocaleLowerCase()){
-        let ans='Please wait for 24 hours you will get the mail';
-return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'How I can Register'.toLocaleLowerCase()){
-        let ans='Need to go the Register page';
-        return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'How I Can Reach the venue from my Location'.toLocaleLowerCase()){
-        let ans='Will be update soon';
-        return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'Who are the Speakers'.toLocaleLowerCase()){
-        let ans='Need to go to speakers page';
-        return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'Can I Register without Accommodation ?'.toLocaleLowerCase()){
-        let ans='Contact below details';
-        return ans;
-    }
-
-    else if(userInput.toLocaleLowerCase() == 'hello'.toLocaleLowerCase()){
-        let ans='How can i help you';
-        return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'hey'.toLocaleLowerCase()){
-        let ans='How can i help you';
-        return ans;
-    }
-    else if(userInput.toLocaleLowerCase() == 'hii'.toLocaleLowerCase()){
-        let ans='How can i help you';
-        return ans;
-    }
-    else{
-        return 'please ask something else'
-    }
+function response(userInput) {
+  if (
+    userInput.toLocaleLowerCase() ==
+    "I Have Registered but did not get the confirmation mail".toLocaleLowerCase()
+  ) {
+    let ans = "Please wait for 24 hours you will get the mail";
+    return ans;
+  } else if (
+    userInput.toLocaleLowerCase() == "How I can Register".toLocaleLowerCase()
+  ) {
+    let ans = "Need to go the Register page";
+    return ans;
+  } else if (
+    userInput.toLocaleLowerCase() ==
+    "How I Can Reach the venue from my Location".toLocaleLowerCase()
+  ) {
+    let ans = "Will be update soon";
+    return ans;
+  } else if (
+    userInput.toLocaleLowerCase() == "Who are the Speakers".toLocaleLowerCase()
+  ) {
+    let ans = "Need to go to speakers page";
+    return ans;
+  } else if (
+    userInput.toLocaleLowerCase() ==
+    "Can I Register without Accommodation ?".toLocaleLowerCase()
+  ) {
+    let ans = "Contact below details";
+    return ans;
+  } else if (userInput.toLocaleLowerCase() == "hello".toLocaleLowerCase()) {
+    let ans = "How can i help you";
+    return ans;
+  } else if (userInput.toLocaleLowerCase() == "hey".toLocaleLowerCase()) {
+    let ans = "How can i help you";
+    return ans;
+  } else if (userInput.toLocaleLowerCase() == "hii".toLocaleLowerCase()) {
+    let ans = "How can i help you";
+    return ans;
+  } else {
+    return "please ask something else";
+  }
 }
-
